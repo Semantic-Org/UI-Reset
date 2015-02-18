2750d806170eb2d653502d1ae18013ca77b39877
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:14:36 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-reset',
+  summary : 'Semantic UI - Reset (official): Single component release of reset',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Reset.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
