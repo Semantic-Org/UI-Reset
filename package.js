eaf2b753366185556aa7bff9a8c850d6ceb26e6d
Pusher Robot <jack@semantic-ui.com>
2015-07-07 17:49:01 -0400

    Updated component to version 2.0.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-reset',
   summary : 'Semantic UI - Reset: Single component release',
-  version : '2.0.1',
+  version : '2.0.2',
   git     : 'git://github.com/Semantic-Org/UI-Reset.git',
 });
 
