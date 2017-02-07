5e7162a63a01c9f389052fa08b25054c09761feb
Jack Lukic <jack@semantic-ui.com>
2017-02-07 10:58:01 -0500

    Updated component to version 2.2.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-reset',
   summary : 'Semantic UI - Reset: Single component release',
-  version : '2.2.6',
+  version : '2.2.7',
   git     : 'git://github.com/Semantic-Org/UI-Reset.git',
 });
 
